5f8c0d5c5642c2789fc91991702139dbf303d522
pastafarista <antonio.cabrera.landin@gmail.com>
2023-12-16 15:44:52 +0100

    fixed: Cambiado el estilo de los formularios

--- src/components/user-register.js
+++ src/components/user-register.js
@@ -54,46 +54,64 @@ export default function Register() {
         <section>
 
             {/*Formulario de registro*/}
-            <div className="w-full bg-blue rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full bg-blue rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 from-gray-900 to-gray-600 bg-gradient-to-r">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                <p className="text-white">Regístrate</p>
-                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
 
-                        <div>
-                            <input onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" required />
-                        </div>
+                {/* Título */}
+                <div>
+                    <p className="text-white text-xl font-bold">Regístrate</p>
+                </div>
 
-                        <div>
-                            <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Correo Electrónico" required />
-                        </div>
-                        <div>
-                            <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Contraseña" x-model="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                        </div>
+                {/* Formulario */}
+                <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
 
-                        <div>
-                            <input onChange={(e) => setAge(e.target.value)} type="number" name="age" id="age" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Edad" required />
-                        </div>
+                    {/* Nombre */}
+                    <div>
+                        <input onChange={(e) => setName(e.target.value)} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Nombre" required />
+                    </div>
 
-                        <div>
-                            <input onChange={(e) => setCity(e.target.value)} type="text" name="city" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ciudad" required />
-                        </div>
+                    {/* Email */}
+                    <div>
+                        <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Correo Electrónico" required />
+                    </div>
 
-                        <div>
-                            <input onChange={(e) => setHobbies(e.target.value)} type="text" name="hobbies" id="hobbies" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Hobbies" required />
-                        </div>
+                    {/* Contraseña */}
+                    <div>
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Contraseña" x-model="password" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
 
-                        <div className="flex items-center">
-                            <input onChange={(e) => setReciveOffers(e.target.value)} type="checkbox" name="reciveOffers" id="reciveOffers" className="rounded text-primary-600 focus:ring-primary-600" />
-                            <label htmlFor="reciveOffers" className="ml-2 block text-sm text-white-900">
-                                Recibir ofertas
-                            </label>
-                        </div>
+                    {/* Edad */}
+                    <div>
+                        <input onChange={(e) => setAge(e.target.value)} type="number" name="age" id="age" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Edad" required />
+                    </div>
 
-                        <div className="flex justify-between">
-                            <Link href="/login-usuarios" className="align-start text-xs font-thin text-white hover:underline">Ya tengo una cuenta</Link>
-                        </div>
-                        <button type="submit" className="w-full text-blue-600 bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none font-medium rounded-full text-sm px-5 py-2.5 text-center">Registrar</button>
-                    </form>
+                    {/* Ciudad */}
+                    <div>
+                        <input onChange={(e) => setCity(e.target.value)} type="text" name="city" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ciudad" required />
+                    </div>
+
+                    {/* Hobbies */}
+                    <div>
+                        <input onChange={(e) => setHobbies(e.target.value)} type="text" name="hobbies" id="hobbies" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Hobbies" required />
+                    </div>
+
+                    {/* Recibir ofertas */}
+                    <div className="flex items-center">
+                        <input onChange={(e) => setReciveOffers(e.target.value)} type="checkbox" name="reciveOffers" id="reciveOffers" className="rounded text-primary-600 focus:ring-primary-600" />
+                        <label htmlFor="reciveOffers" className="ml-2 block text-sm text-white-900">
+                            Recibir ofertas
+                        </label>
+                    </div>
+
+                    {/* Link a login de usuarios */}
+                    <div className="flex justify-between">
+                        <Link href="/login-usuarios" className="align-start text-xs font-thin text-white hover:underline">Ya tengo una cuenta</Link>
+                    </div>
+                    
+                    {/* Botón de registro */}
+                    <button type="submit" className="w-full text-black font-semibold bg-white hover:bg-blue-100 focus:ring-4 focus:outline-none rounded-full text-sm px-5 py-2.5 text-center">Registrar</button>
+                </form>
+
                 </div>
             </div>
         </section>
