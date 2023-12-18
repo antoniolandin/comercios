61440390fe628b5ac4125d75cf05d89e39a23210
pastafarista <antonio.cabrera.landin@gmail.com>
2023-12-18 12:43:37 +0100

    fixed: Cambiado las ciudades a desplegables en el registro de usuario

--- src/components/user-register.js
+++ src/components/user-register.js
@@ -86,9 +86,23 @@ export default function Register() {
                     </div>
 
                     {/* Ciudad */}
-                    <div>
-                        <input onChange={(e) => setCity(e.target.value)} type="text" name="city" id="city" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ciudad" required />
-                    </div>
+                    <div className="flex flex-col space-y-4 mt-5">
+                                <select onChange={(e) => setCity(e.target.value)} name="ciudad" id="ciudad" className="block w-full  py-4 pl-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                                    {/* Placeholder */}
+                                    <option value="" disabled selected hidden>Ciudad</option>
+
+                                    {/* Opciones */}
+                                    <option value="Madrid">Madrid</option>
+                                    <option value="Barcelona">Barcelona</option>
+                                    <option value="Valencia">Valencia</option>
+                                    <option value="Sevilla">Sevilla</option>
+                                    <option value="Alicante">Alicante</option>
+                                    <option value="Málaga">Málaga</option>
+                                    <option value="Jerez">Jerez</option>
+                                    <option value="Oviedo">Oviedo</option>
+                                </select>
+                            </div>
 
                     {/* Hobbies */}
                     <div>
